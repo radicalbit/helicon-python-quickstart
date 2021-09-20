9cdae44c520318a81334f0c8049899ff299d742b
andreatugnoli <andrea.tugnoli@radicalbit.io>
2021-09-20 17:02:17 +0200

    fix install retrieving directly distributed files

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository contains a python publish and subscribe client example.
 
 ## How do I get set up? ##
 
-* Run `pip install -e .` to install the project. 
+* Run `pip install -r requirements.txt` to install the project. 
   To start to publish and/or subscribe to a topic stream you need to populate all the placeholder fields, defined like <placeholder>.
   Then you can just run the `__main__` method inside each client example.
 
--- src/helicon_publish/publish.py
+++ src/helicon_publish/publish.py
@@ -8,12 +8,13 @@ grpcPort = "<grpc-port>"
 clientId = "<client-id>"
 clientSecret = "<client-secret>"
 tenant = "<tenant-name>"
+stream_name = "<stream_name>"
 
 if __name__ == '__main__':
     helicon_client = HeliconPublishClient(keycloakUrl=keycloakUrl, serverHost=grpcHost, serverPort=grpcPort,
                                           clientID=clientId, clientSecret=clientSecret, tenantName=tenant)
 
     payload = f'{{"temperature": 26, "timestamp": {time.time()}}}'
-    helicon_client.write("stream_name", payload)
+    helicon_client.write(stream_name, payload)
 
     helicon_client.close()
