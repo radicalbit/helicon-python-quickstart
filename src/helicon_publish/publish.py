b6551da80eaa2574880d21e0b5ceac585d6936e0
andreatugnoli <andrea.tugnoli@radicalbit.io>
2021-09-24 11:41:42 +0200

    CSH-390: fix publish client constructor arguments

--- src/helicon_publish/publish.py
+++ src/helicon_publish/publish.py
@@ -4,16 +4,16 @@ from helicon_publish.helicon_publish_client import HeliconPublishClient
 
 authorizationServer = "<authorization_server>"
 grpcHost = "<grpc-host>"
-grpcPort = "<grpc-port>"
+grpcPort = 0 # "<grpc-port-as-int>"
 clientId = "<client-id>"
 clientSecret = "<client-secret>"
 tenant = "<tenant-name>"
 stream_name = "<stream_name>"
 
 if __name__ == '__main__':
-    helicon_client = HeliconPublishClient(authorizationServer=authorizationServer, serverHost=grpcHost,
-                                          serverPort=grpcPort, clientID=clientId, clientSecret=clientSecret,
-                                          tenantName=tenant)
+    helicon_client = HeliconPublishClient(authorization_server=authorizationServer, server_host=grpcHost,
+                                          server_port=grpcPort, client_id=clientId, client_secret=clientSecret,
+                                          tenant_name=tenant)
 
     payload = f'{{"temperature": 26, "timestamp": {time.time()}}}'
     helicon_client.write(stream_name, payload)
