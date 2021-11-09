797383821e1906cdb29ce60bd407190716809170
andreatugnoli <andrea.tugnoli@radicalbit.io>
2021-11-09 14:31:55 +0100

    Update python client library version

--- src/helicon_publish/publish.py
+++ src/helicon_publish/publish.py
@@ -1,4 +1,3 @@
-import os
 import time
 
 from helicon_publish.helicon_publish_client import HeliconPublishClient
@@ -12,8 +11,6 @@ tenant = "<tenant-name>"
 stream_name = "<stream-name>"
 
 if __name__ == '__main__':
-    os.environ["GRPC_DEFAULT_SSL_ROOTS_FILE_PATH"] = "path/to/roots.pem"
-
     helicon_client = HeliconPublishClient(authorization_server=authorization_server, server_host=grpc_host,
                                           server_port=grpc_port, client_id=client_id, client_secret=client_secret,
                                           tenant_name=tenant)
